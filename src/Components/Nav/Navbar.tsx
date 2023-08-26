398fb050a0d05fa90e1ae9638431e9d74cecfad8
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2023-08-26 19:49:03 +0300

    Navbar for desktop done

---
+++ src/Components/Nav/Navbar.tsx
@@ -0,0 +1,26 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <a href="/">
+          <h1>
+            <span className="logo-color">&#123; &#125; </span>Aldas.dev
+          </h1>
+        </a>
+      </div>
+      <div className="navbar__links">
+        <div className="navbar__links--buttons">
+          <a href="/">About Me</a>
+          <a href="/">Projects</a>
+          <a href="/">Contacts</a>
+          <a href="/">CV</a>
+        </div>
+      </div>
+      <div className="navbar__footer">
+        <a href="/">Theme</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;

6ae5ae8fa4b8cc6977ad6b69b866abaa2c702238
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2023-09-05 20:04:21 +0300

    Stack icons getting hover effect. But basically responsive is done

--- src/Components/Projects/ProjectsBox/ProjectBox.tsx
+++ src/Components/Projects/ProjectsBox/ProjectBox.tsx
@@ -9,7 +9,7 @@ const ProjectBox: React.FC<ProjectBoxProps> = ({ children, title }) => {
   return (
     <section className="projects">
       <div className="hero-container">
-        <h1>{title}</h1>
+        <h2>{title}</h2>
         <div className="projects__content">{children}</div>
       </div>
     </section>

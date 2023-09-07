741ccba28c8f7034115e68cb4ec99f4177cffe74
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2023-09-07 22:27:04 +0300

    Cheated around with slider.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./Styles/global.scss";
 import Slider from "./Components/Slider/Slider";
+import { SwiperSlide } from "swiper/react";
+import Cucumber from "./Components/Cucumber";
 
 function App() {
   return (
     <>
-      <Slider />
+      {/* <Slider /> */}
+      <Cucumber />
     </>
   );
 }

b2eca6fe09950fddc5758eac5346ab0ded88b7c9
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2023-09-10 21:35:00 +0300

    Added onSlideChange

--- src/Components/Main/Slider/Slider.tsx
+++ src/Components/Main/Slider/Slider.tsx
@@ -1,6 +1,13 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import { Swiper } from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Mousewheel,
+  Keyboard,
+} from "swiper/modules";
 import "swiper/swiper-bundle.css";
 
 interface SliderProp {
@@ -8,18 +15,27 @@ interface SliderProp {
 }
 
 const Slider: React.FC<SliderProp> = ({ children }) => {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+
+  const handleSlideChange = (swiper: any) => {
+    console.log(activeIndex);
+    setActiveIndex((prevSlide) => prevSlide + 1);
+  };
+
   return (
     <Swiper
       //@ts-ignore
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={75}
+      modules={[Navigation, Pagination, Scrollbar, A11y, Mousewheel, Keyboard]}
       slidesPerView={1}
       navigation
+      mousewheel={true}
+      keyboard={true}
       pagination={{ clickable: true }}
-      // onSlideChange={() => console.log("slide change")}
+      loop={true} // Enable infinite loop
       className="slide"
+      onSlideChange={handleSlideChange}
     >
-      {children}
+      <div key={activeIndex}>{children}</div>
     </Swiper>
   );
 };

038efee7c432b3b116514ad14602bc5bda4a51a3
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2023-09-12 21:40:04 +0300

    A bit of improvements. Still want to do some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import "./Styles/global.scss";
 import Main from "./Components/Main";
 
--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -1,8 +1,38 @@
+import { useState, useEffect } from "react";
+import FlyingObject from "./FlyingObject/FlyingObject";
 import Slide from "./Slide/Slide";
 
 function Main() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event: MouseEvent) => {
+    setMousePosition({ x: event.clientX, y: event.clientY });
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  function setBackgroundGradient(mousePosition: { x: number; y: number }) {
+    const centerX = (mousePosition.x / window.innerWidth) * 100;
+    const centerY = (mousePosition.y / window.innerHeight) * 100;
+
+    return `
+      radial-gradient(circle at ${centerX}% ${centerY}%, #3282b8, #0f4c75)
+    `;
+  }
+
   return (
-    <div className="main">
+    <div
+      className="main"
+      style={{ background: setBackgroundGradient(mousePosition) }}
+    >
+      <FlyingObject objectSizeMin={400} objectSizeMax={800} color="#3498db" />
+      <FlyingObject objectSizeMin={50} objectSizeMax={300} color="blue" />
       <div className="main__top">
         <div className="main__top__left">
           <h1>
